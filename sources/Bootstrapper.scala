8429d59a0367b20292501987342cf558c9793370
Damien Simeon <damien.simeon@storyshaper.io>
2017-05-13 11:13:17 +0200

    Created entry point. Need to investigate how to separate this project in several libraries. (Are they nammed package in Java/Scala ?)

---
+++ sources/Bootstrapper.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by damie on 13/05/2017.
+  */
+object Bootstrapper extends App {
+
+  override def main(a: Array[String]) = Initialize()
+
+  def Initialize(): Unit ={
+    // Do stuff
+    println("I'm alive")
+  }
+}
+
